d803cb4aedd4015303025f7dd3b15fa3d2447531
manuudeveloper <ziaulkadri1600@gmail.com>
2023-06-16 16:11:10 +0530

    fixes

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -2,29 +2,34 @@ import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
 const Main = () => {
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		window.location.reload();
-	};
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
 
-	return (
-		<>
-		<div className={styles.main_container}>
-			<nav className={styles.navbar}>
-				<h1>Lost and Found </h1>
-				<button className={styles.white_btn} onClick={handleLogout}>
-					Logout
-				</button>
-			</nav>
-		</div>
-		<main>
-			<h1 style={{textAlign: 'center',marginBottom:'500px'}}>Have you Lost Something?</h1>
-		</main>
-		{/* <footer>
+  return (
+    <>
+      <div className={styles.main_container}>
+        <nav className={styles.navbar}>
+          <h1>Lost and Found </h1>
+          <button className={styles.white_btn} onClick={handleLogout}>
+            Logout
+          </button>
+        </nav>
+      </div>
+      <main>
+        <h1 style={{ textAlign: "center", marginBottom: "500px" }}>
+          Have you Lost Something?
+        </h1>
+      </main>
+      {/* <footer>
 		<h1 style={{textAlign: 'center'}}>Found nothing check again leter.</h1>
 		</footer> */}
-		</>
-	);
+      {/* <footer>
+		<h1 style={{textAlign: 'center'}}>Found nothing check again leter.</h1>
+		</footer> */}
+    </>
+  );
 };
 
 export default Main;
